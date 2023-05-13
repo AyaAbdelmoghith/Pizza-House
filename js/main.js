5d382d4b5d7d8580f38d7d0058628730eeb86f0d
AyaAbdelmoghith <105630381+AyaAbdelmoghith@users.noreply.github.com>
2023-05-13 16:14:03 +0300

    main.js

--- js/main.js
+++ js/main.js
@@ -1,26 +1,7 @@
 
-async function getMenu(){
-}
-function endMenu(){
-  console.log("You have reached the End of the Menu :)");
-}
-function textError(){
-    console.log("Error loading the API data");
-}
-(async function displayMenu(){
-try{
-  await getMenu();
-  endMenu();    
-}
-catch{
-  textError();
-  console.log(typeof(await getMenu()));
-}
-}());//self-invoke function  
-
 const menuIcons=document.querySelectorAll(".menu__icon");
 menuIcons.forEach(menuIcon=>{
 menuIcon.addEventListener("click",()=>{
-  window.location.href="../shop.html";
+  window.location.href="shop.html";
 })
 });
